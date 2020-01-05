397aa8388b1cb018e5fef394a6a49436b5e47f9f
root <root@localhost.localdomain>
2020-01-05 12:28:11 -0500

    kavinda

--- mail.php
+++ mail.php
@@ -16,14 +16,14 @@ $mail = new PHPMailer(true);
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.zoho.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'kavindalakmal01@zohomail.com';                 // SMTP username
-$mail->Password = 'kavindalakmal58';                           // SMTP password
+$mail->Username = 'admin@sinhalahacker.lk';                 // SMTP username
+$mail->Password = 'myface95';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
-$mail->From = 'kavindalakmal01@zohomail.com';
-$mail->FromName = 'My Resume Contact';
-$mail->addAddress('kavindalakmal01@gmail.com');     // Add a recipient               // Name is optional
+$mail->From = 'admin@sinhalahacker.lk';
+$mail->FromName = 'Kavinda Resume Contact';
+$mail->addAddress('shanaka95@gmail.com');     // Add a recipient               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
 //$mail->addBCC('bcc@example.com');
@@ -31,11 +31,11 @@ $mail->addAddress('kavindalakmal01@gmail.com');     // Add a recipient          
 $mail->WordWrap = 50;                                 // Set word wrap to 50 characters  // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Resume Contact';
+$mail->Subject = 'Kavinda Resume Contact';
 //echo 2;
 //echo $_REQUEST['name'];
 
-$a=     $_REQUEST['name']. "<br>" . $_REQUEST['email']. "<br>" . $_REQUEST['title']. "<br>" . $_REQUEST['message']. "<br>" ;
+$a=     $_REQUEST['name']. "<br>" . $_REQUEST['email']. "<br>" . $_REQUEST['title']. "<br>" . $_REQUEST['content']. "<br>" ;
 
 $mail->Body = $a;
 //echo 1;
